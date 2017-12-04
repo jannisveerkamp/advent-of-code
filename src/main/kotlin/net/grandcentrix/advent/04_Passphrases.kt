a75c4dd1d01b508d0a114a5669f531725d441b4f
Jannis Veerkamp <jannis.veerkamp@grandcentrix.net>
2017-12-04 11:23:18 +0100

    Use an extension function

--- src/main/kotlin/net/grandcentrix/advent/04_Passphrases.kt
+++ src/main/kotlin/net/grandcentrix/advent/04_Passphrases.kt
@@ -1,22 +1,25 @@
 package net.grandcentrix.advent
 
+inline fun <T> Iterable<T>.compareItself(compare: (T, T) -> Boolean): Boolean {
+    forEach { outer ->
+        forEach { inner ->
+            if (outer !== inner) {
+                if (compare(outer, inner)) {
+                    return false
+                }
+            }
+        }
+    }
+    return true
+}
+
 fun isPassphraseValid(passphrase: String): Boolean {
     val split = passphrase.split(" ")
     return split.size == split.distinct().size
 }
 
 fun containsNoAnagram(passphrase: String): Boolean {
-    val split = passphrase.split(" ")
-
-    split.forEach { outer ->
-        split.forEach { inner ->
-            if (outer !== inner) {
-                if (outer.toList().sorted() == inner.toList().sorted()) {
-                    return false
-                }
-            }
-        }
+    return passphrase.split(" ").compareItself { s1, s2 ->
+        s1.toList().sorted() == s2.toList().sorted()
     }
-
-    return true
 }
