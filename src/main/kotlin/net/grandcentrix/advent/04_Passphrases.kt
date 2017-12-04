437ce2612c51e510c5b90a067dc85926b89c78ea
Jannis Veerkamp <jannis.veerkamp@grandcentrix.net>
2017-12-04 12:07:14 +0100

    Simplify 4th task

--- src/main/kotlin/net/grandcentrix/advent/04_Passphrases.kt
+++ src/main/kotlin/net/grandcentrix/advent/04_Passphrases.kt
@@ -1,25 +1,15 @@
 package net.grandcentrix.advent
 
-inline fun <T> Iterable<T>.compareItself(compare: (T, T) -> Boolean): Boolean {
-    forEach { outer ->
-        forEach { inner ->
-            if (outer !== inner) {
-                if (compare(outer, inner)) {
-                    return false
-                }
-            }
-        }
-    }
-    return true
-}
-
 fun isPassphraseValid(passphrase: String): Boolean {
     val split = passphrase.split(" ")
     return split.size == split.distinct().size
 }
 
 fun containsNoAnagram(passphrase: String): Boolean {
-    return passphrase.split(" ").compareItself { s1, s2 ->
-        s1.toList().sorted() == s2.toList().sorted()
+    val test = passphrase.split(" ")
+    return !test.any { outer ->
+        test.any { inner ->
+            inner !== outer && inner.toList().sorted() == outer.toList().sorted()
+        }
     }
 }
