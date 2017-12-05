3ad234ca043d64bc43f5e04ff91f379c9f9d8197
Jannis Veerkamp <jannis.veerkamp@grandcentrix.net>
2017-12-05 10:29:53 +0100

    Add simple solution for second task

--- src/main/kotlin/net/grandcentrix/advent/05_Maze.kt
+++ src/main/kotlin/net/grandcentrix/advent/05_Maze.kt
@@ -13,5 +13,17 @@ fun stepsToEscapeMaze(maze: MutableList<Int>): Int {
 }
 
 fun stepsToEscapeAdvancedMaze(maze: MutableList<Int>): Int {
-    return -1
+    var position = 0
+    var steps = 0
+    while (position < maze.size) {
+        val instruction = maze[position]
+        if (instruction >= 3) {
+            maze[position] = instruction - 1
+        } else {
+            maze[position] = instruction + 1
+        }
+        position += instruction
+        steps++
+    }
+    return steps
 }
