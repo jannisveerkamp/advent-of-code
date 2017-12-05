4413975eb7d12f3d39f7ba839f619d084fd85ab4
Jannis Veerkamp <jannis.veerkamp@grandcentrix.net>
2017-12-05 10:54:38 +0100

    Simplify Code

--- src/main/kotlin/net/grandcentrix/advent/05_Maze.kt
+++ src/main/kotlin/net/grandcentrix/advent/05_Maze.kt
@@ -1,27 +1,19 @@
 package net.grandcentrix.advent
 
-fun stepsToEscapeMaze(maze: MutableList<Int>): Int {
-    var position = 0
-    var steps = 0
-    while (position < maze.size) {
-        val instruction = maze[position]
-        maze[position] = instruction + 1
-        position += instruction
-        steps++
-    }
-    return steps
+fun stepsToEscapeMaze(maze: MutableList<Int>): Int = stepsToEscapeMaze(maze) {
+    step -> step + 1
 }
 
-fun stepsToEscapeAdvancedMaze(maze: MutableList<Int>): Int {
+fun stepsToEscapeAdvancedMaze(maze: MutableList<Int>): Int = stepsToEscapeMaze(maze) {
+    step -> if (step >= 3) step - 1 else step + 1
+}
+
+private fun stepsToEscapeMaze(maze: MutableList<Int>, stepChange: (Int) -> Int): Int {
     var position = 0
     var steps = 0
     while (position < maze.size) {
         val instruction = maze[position]
-        if (instruction >= 3) {
-            maze[position] = instruction - 1
-        } else {
-            maze[position] = instruction + 1
-        }
+        maze[position] = stepChange(maze[position])
         position += instruction
         steps++
     }
