e98850a9bf68d6515d42e1cc85d221273577e27e
Jannis Veerkamp <jannis.veerkamp@grandcentrix.net>
2017-12-08 16:07:46 +0100

    Add test input for task 1 Add failing tests Add empty implementation

---
+++ src/main/kotlin/net/grandcentrix/advent/08_Register.kt
@@ -0,0 +1,5 @@
+package net.grandcentrix.advent
+
+fun largestValue(register: List<String>): Int {
+    return -1
+}
---
+++ src/test/kotlin/net/grandcentrix/advent/08_RegisterTest.kt
@@ -0,0 +1,29 @@
+package net.grandcentrix.advent
+
+import org.assertj.core.api.Java6Assertions.assertThat
+import org.junit.*
+import org.junit.runner.*
+import org.junit.runners.*
+
+@RunWith(JUnit4::class)
+class RegisterTest {
+
+    private val simpleTestInput = mutableListOf(
+            "b inc 5 if a > 1",
+            "a inc 1 if b < 5",
+            "c dec -10 if a >= 1",
+            "c inc -20 if c == 10"
+    )
+
+    private val testInput = linesFromResource("08_register_input.txt")
+
+    @Test
+    fun `simpleTestInput has largest register of 1 after process`() {
+        assertThat(largestValue(simpleTestInput)).isEqualTo(1)
+    }
+
+    @Test
+    fun `testInput has largest register of 0 after process`() {
+        assertThat(largestValue(testInput)).isEqualTo(0)
+    }
+}
