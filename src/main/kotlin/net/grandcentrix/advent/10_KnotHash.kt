5affb3fa0b25ddc8827cd8840311f874fc44347d
Jannis Veerkamp <jannis.veerkamp@grandcentrix.net>
2017-12-10 13:31:59 +0100

    Add solution for task 1

--- src/main/kotlin/net/grandcentrix/advent/10_KnotHash.kt
+++ src/main/kotlin/net/grandcentrix/advent/10_KnotHash.kt
@@ -6,5 +6,26 @@ fun knotHash(input: List<Int>, inputLengths: List<Int>): Int {
 }
 
 fun knot(input: List<Int>, inputLengths: List<Int>): List<Int> {
-    return input
+    val currentList = input.toMutableList()
+    var currentPosition = 0
+    var skipSize = 0
+
+    inputLengths.forEach { length ->
+        val start = currentPosition
+        val end = (currentPosition + length) % currentList.size
+
+        val replacement = if (end >= start) {
+            currentList.subList(start, end).reversed()
+        } else {
+            (currentList.subList(start, currentList.size) + currentList.subList(0, end)).reversed()
+        }
+        for (i in 0..(replacement.size - 1)) {
+            currentList[(i + start) % currentList.size] = replacement[i]
+        }
+
+        currentPosition = (currentPosition + length + skipSize) % currentList.size
+        skipSize++
+    }
+
+    return currentList
 }
