93fb636fb595474c02c70aad9b87f34574c11f0e
Jannis Veerkamp <jannis.veerkamp@grandcentrix.net>
2017-12-11 15:18:36 +0100

    Some code style

--- src/main/kotlin/net/grandcentrix/advent/11_HexEd.kt
+++ src/main/kotlin/net/grandcentrix/advent/11_HexEd.kt
@@ -8,35 +8,17 @@ fun hexDistanceAndLargestDistance(input: List<String>): Pair<Int, Int> {
     var x = 0
     var y = 0
     var z = 0
-
     var largestDistance = 0
 
     input.forEach {
+        // "and" is totally useless here - but it looks nice ;-)
         when (it) {
-            "n" -> {
-                y++
-                z--
-            }
-            "s" -> {
-                y--
-                z++
-            }
-            "nw" -> {
-                y++
-                x--
-            }
-            "se" -> {
-                y--
-                x++
-            }
-            "ne" -> {
-                z--
-                x++
-            }
-            "sw" -> {
-                z++
-                x--
-            }
+            "n"  -> y++ and z--
+            "s"  -> y-- and z++
+            "nw" -> y++ and x--
+            "se" -> y-- and x++
+            "ne" -> z-- and x++
+            "sw" -> z++ and x--
         }
         largestDistance = Math.max(distance(x, y, z), largestDistance)
     }
@@ -44,4 +26,4 @@ fun hexDistanceAndLargestDistance(input: List<String>): Pair<Int, Int> {
     return distance(x, y, z) to largestDistance
 }
 
-fun distance(x: Int, y: Int, z: Int): Int = Math.max(Math.max(Math.abs(x), Math.abs(y)), Math.abs(z))
+fun distance(x: Int, y: Int, z: Int): Int = maxOf(maxOf(x, y), z)
