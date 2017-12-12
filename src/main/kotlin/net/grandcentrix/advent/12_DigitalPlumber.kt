258c8f6e75c58eae732abcce3bc1d5ec2aaa4385
Jannis Veerkamp <jannis.veerkamp@grandcentrix.net>
2017-12-12 09:21:12 +0100

    Add test input for task 1 Add empty implementation

---
+++ src/main/kotlin/net/grandcentrix/advent/12_DigitalPlumber.kt
@@ -0,0 +1,5 @@
+package net.grandcentrix.advent
+
+fun connectedPrograms(program: Int, input: List<String>): Int {
+    return -1
+}
---
+++ src/test/kotlin/net/grandcentrix/advent/12_DigitalPlumberTest.kt
@@ -0,0 +1,24 @@
+package net.grandcentrix.advent
+
+import org.assertj.core.api.Java6Assertions.assertThat
+import org.junit.*
+import org.junit.runner.*
+import org.junit.runners.*
+
+@RunWith(JUnit4::class)
+class DigitalPlumberTest {
+
+    private val testInput = linesFromResource("12_digital_plumber_input.txt")
+
+    private val simpleTestInput = linesFromResource("12_digital_plumber_simple_input.txt")
+
+    @Test
+    fun `program 0 from simpleTestInput is connected with 6 programs (including itself)`() {
+        assertThat(connectedPrograms(0, simpleTestInput)).isEqualTo(6)
+    }
+
+    @Test
+    fun `program 0 from testInput is connected with 0 programs (including itself)`() {
+        assertThat(connectedPrograms(0, testInput)).isEqualTo(0)
+    }
+}
