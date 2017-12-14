227fbb426e5108cb6da02815ffceef335e20530d
Jannis Veerkamp <jannis.veerkamp@grandcentrix.net>
2017-12-14 11:28:45 +0100

    Simplify code

--- src/main/kotlin/net/grandcentrix/advent/14_DiskDefragmentation.kt
+++ src/main/kotlin/net/grandcentrix/advent/14_DiskDefragmentation.kt
@@ -5,14 +5,10 @@ import java.lang.Integer.toBinaryString
 
 private const val GRID_SIZE = 128
 
-internal fun squaresUsed(input: String): Int {
-    val grid = buildGrid(input)
-    println(grid.toList().joinToString(separator = "\n"))
-    return grid.sumBy { row -> row.sumBy { it.toString().toInt() } }
-}
+internal fun squaresUsed(input: String) = buildGrid(input).sumBy { row -> row.sum() }
 
 internal fun distinctRegions(input: String): Int {
-    val grid = to2dArray(buildGrid(input))
+    val grid = buildGrid(input)
     var label = 2 // start with a label of 2
 
     grid.forEachIndexed { indexRow, row ->
@@ -45,10 +41,6 @@ private fun markAround(grid: Array<IntArray>, row: Int, column: Int, label: Int)
     }
 }
 
-private fun to2dArray(buildGrid: Sequence<String>): Array<IntArray> {
-    return buildGrid.toList().map { it.map { it.toString().toInt() }.toIntArray() }.toTypedArray()
-}
-
 internal fun toBinary(hexHash: String): String {
     return hexHash.map {
         val hex = parseInt(it.toString(), 16)
@@ -56,8 +48,10 @@ internal fun toBinary(hexHash: String): String {
     }.joinToString(separator = "")
 }
 
-private fun buildGrid(input: String): Sequence<String> {
-    val rows = generateSequence(0, Int::inc).take(GRID_SIZE).map { "$input-$it" }
+private fun buildGrid(input: String): Array<IntArray> {
+    val rows = generateSequence(0, Int::inc).take(GRID_SIZE).map { "$input-$it" }.toList()
     val lengths = IntArray(256) { it -> it }.toList()
-    return rows.map { toBinary(denseHash(it, lengths, 64)) }
+    return rows.map {
+        toBinary(denseHash(it, lengths, 64)).map { it.toString().toInt() }.toIntArray()
+    }.toTypedArray()
 }
