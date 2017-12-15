24ffdf29a9a7b02cd37023b69da446988378eea3
Jannis Veerkamp <jannis.veerkamp@grandcentrix.net>
2017-12-15 11:01:08 +0100

    Add simple solution for task 1

--- src/main/kotlin/net/grandcentrix/advent/15_DuelingGenerators.kt
+++ src/main/kotlin/net/grandcentrix/advent/15_DuelingGenerators.kt
@@ -1,5 +1,32 @@
 package net.grandcentrix.advent
 
+private const val FACTOR_GENERATOR_A = 16_807
+
+private const val FACTOR_GENERATOR_B = 48_271
+
+private const val DIVISOR = 2_147_483_647 // This equals Int.MAX_VALUE!
+
 internal fun duelEqualsCount(startValueGeneratorA: Int, startValueGeneratorB: Int, iterations: Int): Int {
-    return -1
+    var currentA = startValueGeneratorA.toLong()
+    var currentB = startValueGeneratorB.toLong()
+    var matches = 0
+
+    repeat(iterations) {
+        currentA = (currentA * FACTOR_GENERATOR_A) % DIVISOR
+        currentB = (currentB * FACTOR_GENERATOR_B) % DIVISOR
+
+        if (equalsLast16Bits(toBinary(currentA), toBinary(currentB))) {
+            matches++
+        }
+    }
+
+    return matches
+}
+
+internal fun equalsLast16Bits(s1: String, s2: String): Boolean {
+    return s1.takeLast(16) == s2.takeLast(16)
+}
+
+internal fun toBinary(number: Long): String {
+    return java.lang.Integer.toBinaryString(number.toInt()).padStart(32, '0')
 }
--- src/test/kotlin/net/grandcentrix/advent/15_DuelingGeneratorsTest.kt
+++ src/test/kotlin/net/grandcentrix/advent/15_DuelingGeneratorsTest.kt
@@ -9,6 +9,67 @@ import org.junit.runners.*
 class DuelingGeneratorsTest {
 
     @Test
+    fun `binary representation of 1092455 is 00000000000100001010101101100111`() {
+        assertThat(toBinary(1092455)).isEqualTo("00000000000100001010101101100111")
+    }
+
+    @Test
+    fun `binary representation of 430625591 is 00011001101010101101001100110111`() {
+        assertThat(toBinary(430625591)).isEqualTo("00011001101010101101001100110111")
+    }
+
+    @Test
+    fun `binary representation of 1181022009 is 01000110011001001111011100111001`() {
+        assertThat(toBinary(1181022009)).isEqualTo("01000110011001001111011100111001")
+    }
+
+    @Test
+    fun `binary representation of 1233683848 is 01001001100010001000010110001000`() {
+        assertThat(toBinary(1233683848)).isEqualTo("01001001100010001000010110001000")
+    }
+
+    @Test
+    fun `binary representation of 245556042 is 00001110101000101110001101001010`() {
+        assertThat(toBinary(245556042)).isEqualTo("00001110101000101110001101001010")
+    }
+
+    @Test
+    fun `binary representation of 1431495498 is 01010101010100101110001101001010`() {
+        assertThat(toBinary(1431495498)).isEqualTo("01010101010100101110001101001010")
+    }
+
+
+    @Test
+    fun `binary representation of 1744312007 is 01100111111110000001011011000111`() {
+        assertThat(toBinary(1744312007)).isEqualTo("01100111111110000001011011000111")
+    }
+
+    @Test
+    fun `binary representation of 137874439 is 00001000001101111100110000000111`() {
+        assertThat(toBinary(137874439)).isEqualTo("00001000001101111100110000000111")
+    }
+
+    @Test
+    fun `binary representation of 1352636452 is 01010000100111111001100000100100`() {
+        assertThat(toBinary(1352636452)).isEqualTo("01010000100111111001100000100100")
+    }
+
+    @Test
+    fun `binary representation of 285222916 is 00010001000000000010100000000100`() {
+        assertThat(toBinary(285222916)).isEqualTo("00010001000000000010100000000100")
+    }
+
+    @Test
+    fun `The last 16 bits of 1181022009 and 1233683848 are not equal`() {
+        assertThat(equalsLast16Bits(toBinary(1181022009), toBinary(1233683848))).isFalse()
+    }
+
+    @Test
+    fun `The last 16 bits of 245556042 and 1431495498 are equal`() {
+        assertThat(equalsLast16Bits(toBinary(245556042), toBinary(1431495498))).isTrue()
+    }
+
+    @Test
     fun `Dueling 65 and 8921 5 times gives an equals count of 1`() {
         assertThat(duelEqualsCount(65, 8921, 5)).isEqualTo(1)
     }
@@ -19,7 +80,7 @@ class DuelingGeneratorsTest {
     }
 
     @Test
-    fun `Dueling 277 and 349 40_000_000 times gives an equals count of 0`() {
-        assertThat(duelEqualsCount(277, 349, 40_000_000)).isEqualTo(0)
+    fun `Dueling 277 and 349 40_000_000 times gives an equals count of 592`() {
+        assertThat(duelEqualsCount(277, 349, 40_000_000)).isEqualTo(592)
     }
 }
