64f04e12d72412eafa37040f98810cb7dd2f8058
Jannis Veerkamp <jannis.veerkamp@grandcentrix.net>
2017-12-16 14:27:18 +0100

    Add simple solution for task 1

--- src/main/kotlin/net/grandcentrix/advent/16_PermutationPromenade.kt
+++ src/main/kotlin/net/grandcentrix/advent/16_PermutationPromenade.kt
@@ -1,5 +1,38 @@
 package net.grandcentrix.advent
 
 internal fun permutate(input: String, permutations: String): String {
-    return ""
+    val commands = permutations.split(",")
+    var result = input
+
+    commands.forEach {
+        result = when {
+            it.startsWith("s") -> spin(result, it)
+            it.startsWith("x") -> exchange(result, it)
+            it.startsWith("p") -> partner(result, it)
+            else -> it
+        }
+    }
+
+    return result
+}
+
+internal fun spin(input: String, permutation: String): String {
+    val shift = input.length - permutation.substring(1).toInt()
+    return input.substring(shift) + input.substring(0, shift)
+}
+
+internal fun exchange(input: String, permutation: String): String {
+    val pos1 = permutation.substring(1, permutation.indexOf("/")).toInt()
+    val pos2 = permutation.substring(permutation.indexOf("/") + 1).toInt()
+    val builder = StringBuilder(input)
+    val charAtPos1 = builder[pos1]
+    builder.setCharAt(pos1, builder[pos2])
+    builder.setCharAt(pos2, charAtPos1)
+    return builder.toString()
+}
+
+internal fun partner(input: String, permutation: String): String {
+    val char1 = permutation[1]
+    val char2 = permutation[3]
+    return exchange(input, "x" + input.indexOf(char1) + "/" + input.indexOf(char2))
 }
--- src/test/kotlin/net/grandcentrix/advent/16_PermutationPromenade.kt
+++ src/test/kotlin/net/grandcentrix/advent/16_PermutationPromenade.kt
@@ -13,12 +13,32 @@ class PermutationPromenadeTest {
     private val testInput = fromResource("16_permutation_promenade_input.txt")
 
     @Test
+    fun `Spins s1 of "abcde" is "eabcd"`() {
+        assertThat(spin("abcde", "s1")).isEqualTo("eabcd")
+    }
+
+    @Test
+    fun `Spins s3 of "abcde" is "cdeab"`() {
+        assertThat(spin("abcde", "s3")).isEqualTo("cdeab")
+    }
+
+    @Test
+    fun `Exchange x3_4 of "eabcd" is "eabdc"`() {
+        assertThat(exchange("eabcd", "x3/4")).isEqualTo("eabdc")
+    }
+
+    @Test
+    fun `Partner pe_b of "eabdc" is "baedc"`() {
+        assertThat(partner("eabdc", "pe/b")).isEqualTo("baedc")
+    }
+
+    @Test
     fun `Permutation of "abcde" with simpleTestInput is "baedc"`() {
         assertThat(permutate("abcde", simpleTestInput)).isEqualTo("baedc")
     }
 
     @Test
-    fun `Permutation of "abcdefghijklmnop" with testInput is "abcdefghijklmnop"`() {
-        assertThat(permutate("abcdefghijklmnop", testInput)).isEqualTo("abcdefghijklmnop")
+    fun `Permutation of "abcdefghijklmnop" with testInput is "giadhmkpcnbfjelo"`() {
+        assertThat(permutate("abcdefghijklmnop", testInput)).isEqualTo("giadhmkpcnbfjelo")
     }
 }
