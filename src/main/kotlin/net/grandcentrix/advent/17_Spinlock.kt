db87b780c5b2518239bbc86ff1bab2534de3d426
Jannis Veerkamp <jannis.veerkamp@grandcentrix.net>
2017-12-17 17:41:11 +0100

    Add solution for task 2

--- src/main/kotlin/net/grandcentrix/advent/17_Spinlock.kt
+++ src/main/kotlin/net/grandcentrix/advent/17_Spinlock.kt
@@ -4,14 +4,28 @@ private const val INSERTIONS = 2017
 
 internal fun circularSpin(steps: Int): Int {
     val buffer = mutableListOf(0)
-    var currentPosition = 0
-    var currentInsertionValue = 1
+    var position = 0
+    var insertionValue = 1
 
     repeat(INSERTIONS) { _ ->
-        currentPosition = (currentPosition + 1 + steps) % buffer.size
-        buffer.add(currentPosition, currentInsertionValue)
-        currentInsertionValue++
+        position = (position + 1 + steps) % buffer.size
+        buffer.add(position, insertionValue)
+        insertionValue++
     }
 
-    return buffer[(currentPosition + 1) % buffer.size]
+    return buffer[(position + 1) % buffer.size]
+}
+
+internal fun circularSpinValueAfter0(steps: Int, insertions: Int): Int {
+    var position = 0
+    var value = 0
+
+    for (it in 1..insertions) {
+        position = (position + 1 + steps) % it
+        if (position == 0) {
+            value = it
+        }
+    }
+
+    return value
 }
--- src/test/kotlin/net/grandcentrix/advent/17_SpinlockTest.kt
+++ src/test/kotlin/net/grandcentrix/advent/17_SpinlockTest.kt
@@ -13,12 +13,17 @@ class SpinlockTest {
     private val testInput = 337
 
     @Test
-    fun `Circular spinning for simpleTestInput is 638"`() {
+    fun `Circular spinning for simpleTestInput after 2017 insertions is 638"`() {
         assertThat(circularSpin(simpleTestInput)).isEqualTo(638)
     }
 
     @Test
-    fun `Circular spinning for testInput is 600"`() {
+    fun `Circular spinning for testInput after 2017 insertions is 600"`() {
         assertThat(circularSpin(testInput)).isEqualTo(600)
+    }
+
+    @Test
+    fun `Circular spinning for testInput after 50_000_000 insertions is 31220910"`() {
+        assertThat(circularSpinValueAfter0(testInput, 50_000_000)).isEqualTo(31220910)
     }
 }
