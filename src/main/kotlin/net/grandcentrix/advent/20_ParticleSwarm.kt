fcae7051a41d15a93541cfd0bb75e4acbe6eb2b6
Jannis Veerkamp <jannis.veerkamp@grandcentrix.net>
2017-12-20 09:53:17 +0100

    Add simple, but ugly solution for task 1

--- src/main/kotlin/net/grandcentrix/advent/20_ParticleSwarm.kt
+++ src/main/kotlin/net/grandcentrix/advent/20_ParticleSwarm.kt
@@ -1,5 +1,34 @@
 package net.grandcentrix.advent
 
+import kotlin.math.absoluteValue
+
+data class Particle(var x: Long, var y:Long, var z:Long,
+                    var vx: Long, var vy: Long, var vz: Long,
+                    var ax: Long, var ay: Long, var az: Long) {
+    fun distanceToZero() = x.absoluteValue + y.absoluteValue + z.absoluteValue
+}
+
 fun closestParticle(input: List<String>): Int {
-    return -1
+    val particles = input.map { parseParticle(it) }
+
+    repeat(1000) {
+        particles.forEach {
+            it.vx += it.ax
+            it.vy += it.ay
+            it.vz += it.az
+            it.x += it.vx
+            it.y += it.vy
+            it.z += it.vz
+        }
+    }
+
+    return particles.indexOf(particles.minBy { it.distanceToZero() })
+}
+
+fun parseParticle(line: String): Particle {
+    val elements = line.split(", ")
+    val p = elements[0].substring(3, elements[0].length - 1).split(",").map { it.toLong() }
+    val v = elements[1].substring(3, elements[1].length - 1).split(",").map { it.toLong() }
+    val a = elements[2].substring(3, elements[2].length - 1).split(",").map { it.toLong() }
+    return Particle(p[0], p[1], p[2], v[0], v[1], v[2], a[0], a[1], a[2])
 }
--- src/test/kotlin/net/grandcentrix/advent/20_ParticleSwarmTest.kt
+++ src/test/kotlin/net/grandcentrix/advent/20_ParticleSwarmTest.kt
@@ -9,11 +9,17 @@ import org.junit.runners.*
 class ParticleSwarmTest {
 
     private val simpleTestInput = listOf(
-            "p=< 3,0,0>, v=< 2,0,0>, a=<-1,0,0>",
-            "p=< 4,0,0>, v=< 0,0,0>, a=<-2,0,0>"
+            "p=<3,0,0>, v=<2,0,0>, a=<-1,0,0>",
+            "p=<4,0,0>, v=<0,0,0>, a=<-2,0,0>"
     )
 
     private val testInput = linesFromResource("20_particle_swarm_input.txt")
+
+    @Test
+    fun `Test particle parsing`() {
+        assertThat(parseParticle("p=<1,2,3>, v=<-1,-2,-3>, a=<5,-6,7>"))
+                .isEqualTo(Particle(1, 2, 3, -1, -2, -3, 5, -6, 7))
+    }
 
     @Test
     fun `Closest particle to (0,0,0) for simpleTestInput in the long run is #0`() {
