bc30cb17b783c7bd3493ae4456ad042ca894a907
Jannis Veerkamp <jannis.veerkamp@grandcentrix.net>
2017-12-22 14:50:57 +0100

    Add solution for task 1

--- src/main/kotlin/net/grandcentrix/advent/21_FractalArt.kt
+++ src/main/kotlin/net/grandcentrix/advent/21_FractalArt.kt
@@ -1,7 +1,105 @@
 package net.grandcentrix.advent
 
-private val START = listOf(".#.", "..#", "###")
+private val START = listOf(
+        ".#.",
+        "..#",
+        "###"
+)
 
-fun doArt(input: List<String>, iterations: Int): Int {
-    return -1
+internal class Grid(var items: Array<Array<GridPiece>>) {
+    fun flatten() {
+        val flat = mutableListOf<String>()
+        items.forEach { row ->
+            flat.add(row.joinToString("") { it.rows[0] })
+            flat.add(row.joinToString("") { it.rows[1] })
+            flat.add(row.joinToString("") { it.rows[2] })
+            if (items[0][0].rows.size == 4) {
+                flat.add(row.joinToString("") { it.rows[3] })
+            }
+        }
+        items = carveGrid(flat)
+    }
+
+    fun grow(rules: Map<String, String>) {
+        items.forEach { row ->
+            row.forEach {
+                it.grow(rules)
+            }
+        }
+    }
 }
+
+class GridPiece(var rows: List<String>) {
+    fun grow(rules: Map<String, String>) {
+        rows = applyRule(rules, rows.joinToString("/")).split("/")
+    }
+}
+
+internal fun carveGrid(input: List<String>): Array<Array<GridPiece>> {
+    val size = if (input.size % 2 == 0) 2 else 3
+    return input.chunked(size).map { rows ->
+        val list = rows.map { it.chunked(size) }
+        (0 until list[0].size).map { it -> GridPiece(list.map { it2 -> it2[it] }) }.toTypedArray()
+    }.toTypedArray()
+}
+
+internal fun doArt(input: List<String>, iterations: Int): Int {
+    val rules = parseRuleInput(input)
+    val grid = Grid(arrayOf(arrayOf(GridPiece(START))))
+
+    repeat(iterations) {
+        grid.grow(rules)
+        grid.flatten()
+    }
+    return grid.items.sumBy { it.sumBy { it.rows.sumBy { it.sumBy { if (it == '#') 1 else 0 } } } }
+}
+
+internal fun applyRule(rules: Map<String, String>, sequence: String): String {
+    // No action needed
+    if (rules.containsKey(sequence)) {
+        return rules[sequence]!!
+    }
+    // Flip horizontally
+    val flipH = sequence.split("/").map { it.reversed() }.joinToString("/")
+    if (rules.containsKey(flipH)) {
+        return rules[flipH]!!
+    }
+    // Flip vertically
+    val flipV = sequence.split("/").reversed().joinToString("/")
+    if (rules.containsKey(flipV)) {
+        return rules[flipV]!!
+    }
+    // Rotations
+    var rotation = sequence
+    repeat(3) {
+        rotation = rotateArray(rotation.split("/").toTypedArray()).joinToString("/") { it.joinToString("") }
+        if (rules.containsKey(rotation)) {
+            return rules[rotation]!!
+        }
+        // Flip horizontally
+        val flipHR = rotation.split("/").map { it.reversed() }.joinToString("/")
+        if (rules.containsKey(flipHR)) {
+            return rules[flipHR]!!
+        }
+        // Flip vertically
+        val flipVR = rotation.split("/").reversed().joinToString("/")
+        if (rules.containsKey(flipVR)) {
+            return rules[flipVR]!!
+        }
+    }
+
+    throw IllegalArgumentException("no matching rule found for $sequence :(")
+}
+
+internal fun rotateArray(input: Array<String>): Array<CharArray> {
+    val n = input.size
+    val newArray = Array(n) { CharArray(n) }
+    for (i in 0 until n) {
+        for (j in 0 until n) {
+            newArray[i][j] = input[n - 1 - j][i]
+        }
+    }
+    return newArray
+}
+
+private fun parseRuleInput(input: List<String>) = input.map { it.split(" => ") }.associate { it[0] to it[1] }
--- src/test/kotlin/net/grandcentrix/advent/21_FractalArtTest.kt
+++ src/test/kotlin/net/grandcentrix/advent/21_FractalArtTest.kt
@@ -16,12 +16,66 @@ class FractalArtTest {
     private val testInput = linesFromResource("21_fractal_art_input.txt")
 
     @Test
-    fun `After 2 iterations 5 pixels of the simpleTestInout are on`() {
-        assertThat(doArt(simpleTestInput, 2)).isEqualTo(5)
+    fun `Rotation of size 2`() {
+        assertThat(rotateArray(arrayOf("12", "34"))).isEqualTo(arrayOf(arrayOf('3', '1'), arrayOf('4', '2')))
     }
 
     @Test
-    fun `After 5 iterations 0 pixels of the testInput are on`() {
-        assertThat(doArt(testInput, 5)).isEqualTo(5)
+    fun `Rotation of size 3`() {
+        assertThat(rotateArray(arrayOf("123", "456", "789")))
+                .isEqualTo(arrayOf(arrayOf('7', '4', '1'), arrayOf('8', '5', '2'), arrayOf('9', '6', '3')))
+    }
+
+    @Test
+    fun `After 2 iterations 12 pixels of the simpleTestInout are on`() {
+        assertThat(doArt(simpleTestInput, 2)).isEqualTo(12)
+    }
+
+    @Test
+    fun `After 5 iterations 190 pixels of the testInput are on`() {
+        assertThat(doArt(testInput, 5)).isEqualTo(190)
+    }
+
+    @Test
+    fun `After 18 iterations 2335049 pixels of the testInput are on`() {
+        assertThat(doArt(testInput, 18)).isEqualTo(2335049)
+    }
+
+    @Test
+    fun `Carve grid with a size of 3`() {
+        val grid = carveGrid(listOf("123", "456", "789"))
+        assertThat(grid.size).isEqualTo(1)
+        assertThat(grid[0].size).isEqualTo(1)
+        assertThat(grid[0][0].rows).isEqualTo(listOf("123", "456", "789"))
+    }
+
+    @Test
+    fun `Carve grid with a size of 4`() {
+        val grid = carveGrid(listOf("1234", "5678", "9abc", "defg"))
+        assertThat(grid.size).isEqualTo(2)
+        assertThat(grid[0].size).isEqualTo(2)
+        assertThat(grid[1].size).isEqualTo(2)
+        assertThat(grid[0][0].rows).isEqualTo(listOf("12", "56"))
+        assertThat(grid[0][1].rows).isEqualTo(listOf("34", "78"))
+        assertThat(grid[1][0].rows).isEqualTo(listOf("9a", "de"))
+        assertThat(grid[1][1].rows).isEqualTo(listOf("bc", "fg"))
+    }
+
+    @Test
+    fun `Carve grid with a size of 6`() {
+        val grid = carveGrid(listOf("123456", "789abc", "defghi", "jklmno", "pqrstu", "vwxyz0"))
+        assertThat(grid.size).isEqualTo(3)
+        assertThat(grid[0].size).isEqualTo(3)
+        assertThat(grid[1].size).isEqualTo(3)
+        assertThat(grid[2].size).isEqualTo(3)
+        assertThat(grid[0][0].rows).isEqualTo(listOf("12", "78"))
+        assertThat(grid[0][1].rows).isEqualTo(listOf("34", "9a"))
+        assertThat(grid[0][2].rows).isEqualTo(listOf("56", "bc"))
+        assertThat(grid[1][0].rows).isEqualTo(listOf("de", "jk"))
+        assertThat(grid[1][1].rows).isEqualTo(listOf("fg", "lm"))
+        assertThat(grid[1][2].rows).isEqualTo(listOf("hi", "no"))
+        assertThat(grid[2][0].rows).isEqualTo(listOf("pq", "vw"))
+        assertThat(grid[2][1].rows).isEqualTo(listOf("rs", "xy"))
+        assertThat(grid[2][2].rows).isEqualTo(listOf("tu", "z0"))
     }
 }
