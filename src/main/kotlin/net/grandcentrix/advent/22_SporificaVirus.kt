e2ec21def0f43aa113124d3786e592148078a61e
Jannis Veerkamp <jannis.veerkamp@grandcentrix.net>
2017-12-22 16:42:47 +0100

    Add solution for task 2

--- src/main/kotlin/net/grandcentrix/advent/22_SporificaVirus.kt
+++ src/main/kotlin/net/grandcentrix/advent/22_SporificaVirus.kt
@@ -4,6 +4,35 @@ import net.grandcentrix.advent.Dir.DOWN
 import net.grandcentrix.advent.Dir.LEFT
 import net.grandcentrix.advent.Dir.RIGHT
 import net.grandcentrix.advent.Dir.UP
+import net.grandcentrix.advent.InfectionState.CLEAN
+import net.grandcentrix.advent.InfectionState.FLAGGED
+import net.grandcentrix.advent.InfectionState.INFECTED
+import net.grandcentrix.advent.InfectionState.WEAKENED
+import kotlin.math.sqrt
+
+enum class InfectionState {
+    CLEAN,
+    WEAKENED,
+    INFECTED,
+    FLAGGED;
+
+    fun simpleEncounter(): InfectionState {
+        return when (this) {
+            CLEAN -> INFECTED
+            INFECTED -> CLEAN
+            else -> this
+        }
+    }
+
+    fun enounter(): InfectionState {
+        return when (this) {
+            CLEAN -> WEAKENED
+            WEAKENED -> INFECTED
+            INFECTED -> FLAGGED
+            FLAGGED -> CLEAN
+        }
+    }
+}
 
 enum class Dir {
     UP,
@@ -11,24 +40,52 @@ enum class Dir {
     LEFT,
     RIGHT;
 
-    fun turn(infected: Boolean): Dir {
+    fun turnSimple(infected: InfectionState) = turn(infected == INFECTED)
+
+    fun turnComplex(infected: InfectionState): Dir {
+        return when (infected) {
+            CLEAN -> turn(false)
+            INFECTED -> turn(true)
+            WEAKENED -> this
+            FLAGGED -> when (this) {
+                UP -> DOWN
+                DOWN -> UP
+                LEFT -> RIGHT
+                RIGHT -> LEFT
+            }
+        }
+    }
+
+    private fun turn(right: Boolean): Dir {
         return when (this) {
-            UP -> if (infected) RIGHT else LEFT
-            DOWN -> if (infected) LEFT else RIGHT
-            LEFT -> if (infected) UP else DOWN
-            RIGHT -> if (infected) DOWN else UP
+            UP -> if (right) RIGHT else LEFT
+            DOWN -> if (right) LEFT else RIGHT
+            LEFT -> if (right) UP else DOWN
+            RIGHT -> if (right) DOWN else UP
         }
     }
 }
 
-internal fun bulkBurst(input: List<String>, iterations: Int): Int {
-    val gridSize = iterations / 4 + input.size
-    val grid: Array<BooleanArray> = Array(gridSize) { BooleanArray(gridSize) { false } }
+internal fun bulkBurstSimple(input: List<String>, iterations: Int): Int {
+    return bulkBurst(input, iterations, { it.simpleEncounter() }, { dir, state -> dir.turnSimple(state) },
+            iterations / 4 + input.size * 2)
+}
+
+internal fun bulkBurstComplex(input: List<String>, iterations: Int): Int {
+    return bulkBurst(input, iterations, { it.enounter() }, { dir, state -> dir.turnComplex(state) },
+            sqrt(iterations.toDouble()).toInt() / 2 + input.size * 2)
+}
+
+internal fun bulkBurst(input: List<String>, iterations: Int, encounter: (InfectionState) -> InfectionState,
+                       turn: (Dir, InfectionState) -> Dir, gridSize: Int): Int {
+    val grid: Array<Array<InfectionState>> = Array(gridSize) { Array(gridSize) { CLEAN } }
 
     val offset = gridSize / 2 - input.size / 2
     input.forEachIndexed { indexRow, row ->
         row.forEachIndexed { indexColumn, value ->
-            grid[indexRow + offset][indexColumn + offset] = value == '#'
+            if (value == '#') {
+                grid[indexRow + offset][indexColumn + offset] = INFECTED
+            }
         }
     }
 
@@ -39,11 +96,12 @@ internal fun bulkBurst(input: List<String>, iterations: Int): Int {
 
     repeat(iterations) {
         // 1. Turn
-        direction = direction.turn(grid[posY][posX])
+        //direction = direction.turn(grid[posY][posX])
+        direction = turn(direction, grid[posY][posX])
 
         // 2. Clean/infect
-        grid[posY][posX] = !grid[posY][posX]
-        if (grid[posY][posX]) {
+        grid[posY][posX] = encounter(grid[posY][posX])
+        if (grid[posY][posX] == INFECTED) {
             infectionCount++
         }
 
--- src/test/kotlin/net/grandcentrix/advent/22_SporificaVirusTest.kt
+++ src/test/kotlin/net/grandcentrix/advent/22_SporificaVirusTest.kt
@@ -44,21 +44,36 @@ class SporificaVirusTest {
 
     @Test
     fun `7 bursts on simpleTestInput caused 5 infections`() {
-        assertThat(bulkBurst(simpleTestInput, 7)).isEqualTo(5)
+        assertThat(bulkBurstSimple(simpleTestInput, 7)).isEqualTo(5)
     }
 
     @Test
     fun `70 bursts on simpleTestInput caused 41 infections`() {
-        assertThat(bulkBurst(simpleTestInput, 70)).isEqualTo(41)
+        assertThat(bulkBurstSimple(simpleTestInput, 70)).isEqualTo(41)
     }
 
     @Test
     fun `10000 bursts on simpleTestInput caused 5587 infections`() {
-        assertThat(bulkBurst(simpleTestInput, 10000)).isEqualTo(5587)
+        assertThat(bulkBurstSimple(simpleTestInput, 10000)).isEqualTo(5587)
     }
 
     @Test
     fun `10000 bursts on testInput caused 5322 infections`() {
-        assertThat(bulkBurst(testInput, 10000)).isEqualTo(5322)
+        assertThat(bulkBurstSimple(testInput, 10000)).isEqualTo(5322)
+    }
+
+    @Test
+    fun `100 bursts on simpleTestInput caused 26 infections with the complex mode`() {
+        assertThat(bulkBurstComplex(simpleTestInput, 100)).isEqualTo(26)
+    }
+
+    @Test
+    fun `10000000 bursts on simpleTestInput caused 2511944 infections with the complex mode`() {
+        assertThat(bulkBurstComplex(simpleTestInput, 10000000)).isEqualTo(2511944)
+    }
+
+    @Test
+    fun `10000000 bursts on testInput caused 2512079 infections with the complex mode`() {
+        assertThat(bulkBurstComplex(testInput, 10000000)).isEqualTo(2512079)
     }
 }
