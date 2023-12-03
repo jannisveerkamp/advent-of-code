5c9a7567c3e3de35b5303ac05b64727d95de424e
Jannis Veerkamp <jannis.veerkamp@deepl.com>
2023-12-03 20:07:54 +0100

    wrong sample

--- 2023/src/main/kotlin/Day03.kt
+++ 2023/src/main/kotlin/Day03.kt
@@ -61,13 +61,67 @@ private fun solveDay03a(input: String): Int {
     return numberSum
 }
 
+private fun solveDay03b(input: String): Int {
+    val grid = parseGrid(input)
+    val xMax = grid.first().size - 1
+    val yMax = grid.size - 1
+
+    grid.forEachIndexed { y, line ->
+        line.forEachIndexed { x, cell ->
+            if (cell.value == '*') {
+                if (x > 0 && y > 0 && grid[y - 1][x - 1].value.isDigit()) grid[y - 1][x - 1].marked = true
+                if (x > 0 && grid[y][x - 1].value.isDigit()) grid[y][x - 1].marked = true
+                if (y > 0 && grid[y - 1][x].value.isDigit()) grid[y - 1][x].marked = true
+                if (x < xMax && y < yMax && grid[y + 1][x + 1].value.isDigit()) grid[y + 1][x + 1].marked = true
+                if (x < xMax && grid[y][x + 1].value.isDigit()) grid[y][x + 1].marked = true
+                if (y < yMax && grid[y + 1][x].value.isDigit()) grid[y + 1][x].marked = true
+                if (x < xMax && y > 0 && grid[y - 1][x + 1].value.isDigit()) grid[y - 1][x + 1].marked = true
+                if (x > 0 && y < yMax && grid[y + 1][x - 1].value.isDigit()) grid[y + 1][x - 1].marked = true
+            }
+        }
+    }
+
+    var numberSum = 0
+
+    // yes, yes...
+    repeat(2) {
+        grid.forEachIndexed { y, line ->
+            line.forEachIndexed { x, cell ->
+                if (cell.marked) {
+                    if (x > 0 && grid[y][x - 1].value.isDigit()) grid[y][x - 1].marked = true
+                    if (x < xMax && grid[y][x + 1].value.isDigit()) grid[y][x + 1].marked = true
+                }
+            }
+        }
+    }
+
+    val regex = "(\\d+)".toRegex()
+
+    grid.forEach { line ->
+        val result = regex.findAll(line.joinToString("") {
+            if (it.marked || !it.value.isDigit()) {
+                it.value.toString()
+            } else {
+                ""
+            }
+        }
+        )
+        result.forEach {
+            numberSum += it.value.toInt()
+        }
+    }
+
+    return numberSum
+}
+
+
 fun main() {
     val inputExample = readFile("day03_example_2.txt")
     val inputTask = readFile("day03.txt")
 
     println("Solution for task 1 example: ${solveDay03a(inputExample)}") // 4361
     println("Solution for task 1 task:    ${solveDay03a(inputTask)}") // 530849
-    println("Solution for task 2 example: ${solveDay03a(inputExample)}") // ???
-    println("Solution for task 2 task:    ${solveDay03a(inputTask)}") // ???
+    println("Solution for task 2 example: ${solveDay03b(inputExample)}") // 467835
+    println("Solution for task 2 task:    ${solveDay03b(inputTask)}") // ???
 }
 
