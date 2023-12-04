4c5e630fffb16e85234901926eeb1cbcc4aa6e6e
Jannis Veerkamp <jannis.veerkamp@deepl.com>
2023-12-04 09:20:28 +0100

    Add day 04 example data

---
+++ 2023/src/main/kotlin/Day04.kt
@@ -0,0 +1,18 @@
+private fun solveDay04a(input: String): Int {
+    return 0
+}
+
+private fun solveDay04b(input: String): Int {
+    return 0
+}
+
+fun main() {
+    val inputExample = readFile("day04_example.txt")
+    val inputTask = readFile("day04.txt")
+
+    println("Solution for task 1 example: ${solveDay04a(inputExample)}") // 13
+    println("Solution for task 1 task:    ${solveDay04a(inputTask)}") // ???
+    println("Solution for task 2 example: ${solveDay04b(inputExample)}") // ???
+    println("Solution for task 2 task:    ${solveDay04b(inputTask)}") // ???
+}
+
