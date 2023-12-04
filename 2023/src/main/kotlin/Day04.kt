724db3b8d186e82e14c89ac503c4ed5af7139b96
Jannis Veerkamp <jannis.veerkamp@deepl.com>
2023-12-04 09:36:03 +0100

    Solution for day 4 part 1

--- 2023/src/main/kotlin/Day04.kt
+++ 2023/src/main/kotlin/Day04.kt
@@ -1,5 +1,18 @@
-private fun solveDay04a(input: String): Int {
-    return 0
+import kotlin.math.pow
+
+private fun solveDay04a(input: String): Int = input.split("\n").sumOf { line ->
+    val (_, numbers) = line.split(": ")
+    val (winning, my) = numbers.replace("  ", " ").split(" | ")
+    val winningNumbers = winning.split(" ")
+    val myNumbers = my.split(" ")
+
+    val myWinningNumbers = myNumbers.count { number ->
+        winningNumbers.contains(number)
+    }
+    when (myWinningNumbers) {
+        0, 1 -> myWinningNumbers
+        else -> 2.toDouble().pow(myWinningNumbers - 1).toInt()
+    }
 }
 
 private fun solveDay04b(input: String): Int {
@@ -11,8 +24,8 @@ fun main() {
     val inputTask = readFile("day04.txt")
 
     println("Solution for task 1 example: ${solveDay04a(inputExample)}") // 13
-    println("Solution for task 1 task:    ${solveDay04a(inputTask)}") // ???
-    println("Solution for task 2 example: ${solveDay04b(inputExample)}") // ???
+    println("Solution for task 1 task:    ${solveDay04a(inputTask)}") // 25004
+    println("Solution for task 2 example: ${solveDay04b(inputExample)}") // 30
     println("Solution for task 2 task:    ${solveDay04b(inputTask)}") // ???
 }
 
