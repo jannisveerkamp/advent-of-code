d43aa0fe856951aa510988f671a9c676c34e8044
Jannis Veerkamp <jannis.veerkamp@deepl.com>
2023-12-04 09:48:48 +0100

    Solution for day 4 part 2

--- 2023/src/main/kotlin/Day04.kt
+++ 2023/src/main/kotlin/Day04.kt
@@ -16,7 +16,23 @@ private fun solveDay04a(input: String): Int = input.split("\n").sumOf { line ->
 }
 
 private fun solveDay04b(input: String): Int {
-    return 0
+    val extraCopies = mutableMapOf<Int, Int>()
+    return input.split("\n").sumOf { line ->
+        val (card, numbers) = line.split(": ")
+        val cardNumber = card.removePrefix("Card ").trim().toInt()
+        val (winning, my) = numbers.replace("  ", " ").split(" | ")
+        val winningNumbers = winning.split(" ")
+        val myNumbers = my.split(" ")
+        val currentExtraCopies = extraCopies[cardNumber] ?: 0
+
+        val myWinningNumbers = myNumbers.count { number ->
+            winningNumbers.contains(number)
+        }
+        (0 until myWinningNumbers).forEach {
+            extraCopies[cardNumber + it + 1] = (extraCopies[cardNumber + it + 1] ?: 0) + 1 + currentExtraCopies
+        }
+        currentExtraCopies + 1
+    }
 }
 
 fun main() {
@@ -26,6 +42,6 @@ fun main() {
     println("Solution for task 1 example: ${solveDay04a(inputExample)}") // 13
     println("Solution for task 1 task:    ${solveDay04a(inputTask)}") // 25004
     println("Solution for task 2 example: ${solveDay04b(inputExample)}") // 30
-    println("Solution for task 2 task:    ${solveDay04b(inputTask)}") // ???
+    println("Solution for task 2 task:    ${solveDay04b(inputTask)}") // 14427616
 }
 
