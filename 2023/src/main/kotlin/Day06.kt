b7dae0563b835795077e112b7637163e338e9596
Jannis Veerkamp <jannis.veerkamp@deepl.com>
2023-12-05 17:00:00 +0100

    Add empty day 06

---
+++ 2023/src/main/kotlin/Day06.kt
@@ -0,0 +1,18 @@
+private fun solveDay06a(input: String): Long {
+    return 0
+}
+
+private fun solveDay06b(input: String): Long {
+    return 0
+}
+
+fun main() {
+    val inputExample = readFile("day06_example.txt")
+    val inputTask = readFile("day06.txt")
+
+    println("Solution for task 1 example: ${solveDay06a(inputExample)}") // 35
+    println("Solution for task 1 task:    ${solveDay06a(inputTask)}") // 226172555
+    println("Solution for task 2 example: ${solveDay06b(inputExample)}") // 46
+    println("Solution for task 2 task:    ${solveDay06b(inputTask)}") // 47909639, takes 20 minutes :-D
+}
+
