d29c4d05d3a42aa552c780bd01e29586d98d2905
Jannis Veerkamp <jannis.veerkamp@deepl.com>
2023-12-06 09:34:13 +0100

    Parse race

--- 2023/src/main/kotlin/Day06.kt
+++ 2023/src/main/kotlin/Day06.kt
@@ -1,4 +1,12 @@
+private fun parseRace(input: String): Map<Int, Int> {
+    val lines = input.split("\n").map { line ->
+        line.split("\\s+".toRegex()).drop(1).map { it.toInt() }
+    }
+    return lines[0].zip(lines[1]).associate { it }
+}
+
 private fun solveDay06a(input: String): Long {
+    val race = parseRace(input)
     return 0
 }
 
@@ -10,9 +18,9 @@ fun main() {
     val inputExample = readFile("day06_example.txt")
     val inputTask = readFile("day06.txt")
 
-    println("Solution for task 1 example: ${solveDay06a(inputExample)}") // 35
-    println("Solution for task 1 task:    ${solveDay06a(inputTask)}") // 226172555
-    println("Solution for task 2 example: ${solveDay06b(inputExample)}") // 46
-    println("Solution for task 2 task:    ${solveDay06b(inputTask)}") // 47909639, takes 20 minutes :-D
+    println("Solution for task 1 example: ${solveDay06a(inputExample)}") // 288
+    println("Solution for task 1 task:    ${solveDay06a(inputTask)}") // ???
+    println("Solution for task 2 example: ${solveDay06b(inputExample)}") // ???
+    println("Solution for task 2 task:    ${solveDay06b(inputTask)}") // ???
 }
 
