55676e209d028a8d9a3f44365dd5b8f77ebabaa2
Jannis Veerkamp <jannis.veerkamp@deepl.com>
2023-12-07 09:02:10 +0100

    Day 7 part 2

--- 2023/src/main/kotlin/Day07.kt
+++ 2023/src/main/kotlin/Day07.kt
@@ -1,36 +1,37 @@
 import kotlin.math.pow
 
-private data class Hand(val value: String) {
+private data class Hand(val value: String, val withJokers: Boolean) {
     val cards = value.toSet()
-    val hasFiveOfAKind = cards.size == 1
-    val hasFourOfAKind = cards.any { card -> value.count { it == card } == 4 }
-    val hasFullHouse =
-        cards.size == 2 && (value.count { it == cards.first() } == 2 || value.count { it == cards.first() } == 3)
-    val hasThreeOfAKind = cards.any { card -> value.count { it == card } == 3 }
-    val hasTwoPairs = cards.count { card -> value.count { it == card } == 2 } == 2
-    val hasOnePair = cards.size < 5
+    val countOfCards: List<Int> = if (withJokers) {
+        cards.minus('J').map { card -> value.count { it == card } }.sorted().reversed()
+    } else {
+        cards.map { card -> value.count { it == card } }.sorted().reversed()
+    }
+    val numberOfJokers = if (withJokers) value.count { it == 'J' } else 0
+
+    val cardValue: Int = when {
+        numberOfJokers == 5 || countOfCards[0] + numberOfJokers == 5 -> 6
+        countOfCards[0] + numberOfJokers == 4 -> 5
+        (countOfCards[0] + numberOfJokers == 3 && countOfCards[1] == 2) ||
+                (countOfCards[0] == 3 && countOfCards[1] + numberOfJokers == 2) -> 4
+
+        countOfCards[0] + numberOfJokers == 3 -> 3
+        countOfCards[0] == 2 && countOfCards[1] + numberOfJokers == 2 -> 2
+        countOfCards[0] + numberOfJokers == 2 -> 1
+        else -> 0
+    }
 
     val singleCardValue: Int = value.reversed().mapIndexed { index, char ->
         20.0.pow(index).toInt() * when {
             char.isDigit() -> char.digitToInt()
             char == 'T' -> 10
-            char == 'J' -> 11
+            char == 'J' -> if (withJokers) 1 else 11
             char == 'Q' -> 12
             char == 'K' -> 13
             char == 'A' -> 14
             else -> error("Unknown card: $char")
         }
     }.sum()
-
-    val cardValue: Int = when {
-        hasFiveOfAKind -> 6
-        hasFourOfAKind -> 5
-        hasFullHouse -> 4
-        hasThreeOfAKind -> 3
-        hasTwoPairs -> 2
-        hasOnePair -> 1
-        else -> 0
-    }
 
     fun compare(other: Hand): Int {
         val compare = cardValue - other.cardValue
@@ -45,11 +46,9 @@ private data class Hand(val value: String) {
 private fun solveDay07a(input: String): Int {
     val deck = input.split("\n").associate {
         val (hand, rank) = it.split(" ")
-        Hand(hand) to rank.toInt()
+        Hand(hand, false) to rank.toInt()
     }
-    val sorted = deck.toSortedMap(Comparator { hand1, hand2 ->
-        hand1.compare(hand2)
-    })
+    val sorted = deck.toSortedMap { hand1, hand2 -> hand1.compare(hand2) }
     var sum = 0
     sorted.onEachIndexed { index, entry ->
         sum += (index + 1) * entry.value
@@ -58,7 +57,16 @@ private fun solveDay07a(input: String): Int {
 }
 
 private fun solveDay07b(input: String): Int {
-    return 0
+    val deck = input.split("\n").associate {
+        val (hand, rank) = it.split(" ")
+        Hand(hand, true) to rank.toInt()
+    }
+    val sorted = deck.toSortedMap { hand1, hand2 -> hand1.compare(hand2) }
+    var sum = 0
+    sorted.onEachIndexed { index, entry ->
+        sum += (index + 1) * entry.value
+    }
+    return sum
 }
 
 fun main() {
@@ -68,6 +76,6 @@ fun main() {
     println("Solution for task 1 example: ${solveDay07a(inputExample)}") // 6440
     println("Solution for task 1 task:    ${solveDay07a(inputTask)}") // 253954294
     println("Solution for task 2 example: ${solveDay07b(inputExample)}") // 5905
-    println("Solution for task 2 task:    ${solveDay07b(inputTask)}") // ???
+    println("Solution for task 2 task:    ${solveDay07b(inputTask)}") // 254837398
 }
 
