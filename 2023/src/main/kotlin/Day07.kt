bce5d9c78ee38408fca12bc0f403f8154557756a
Jannis Veerkamp <jannis.veerkamp@deepl.com>
2023-12-07 09:14:16 +0100

    Improve code

--- 2023/src/main/kotlin/Day07.kt
+++ 2023/src/main/kotlin/Day07.kt
@@ -1,27 +1,22 @@
 import kotlin.math.pow
 
-private data class Hand(val value: String, val withJokers: Boolean) {
-    val cards = value.toSet()
-    val countOfCards: List<Int> = if (withJokers) {
-        cards.minus('J').map { card -> value.count { it == card } }.sorted().reversed()
-    } else {
-        cards.map { card -> value.count { it == card } }.sorted().reversed()
-    }
-    val numberOfJokers = if (withJokers) value.count { it == 'J' } else 0
+private data class Hand(val source: String, val withJokers: Boolean) {
 
-    val cardValue: Int = when {
-        numberOfJokers == 5 || countOfCards[0] + numberOfJokers == 5 -> 6
-        countOfCards[0] + numberOfJokers == 4 -> 5
-        (countOfCards[0] + numberOfJokers == 3 && countOfCards[1] == 2) ||
-                (countOfCards[0] == 3 && countOfCards[1] + numberOfJokers == 2) -> 4
+    val cards = if (withJokers) source.toSet().minus('J') else source.toSet()
+    val count: List<Int> = cards.map { card -> source.count { it == card } }.sortedDescending()
+    val jokers = if (withJokers) source.count { it == 'J' } else 0
 
-        countOfCards[0] + numberOfJokers == 3 -> 3
-        countOfCards[0] == 2 && countOfCards[1] + numberOfJokers == 2 -> 2
-        countOfCards[0] + numberOfJokers == 2 -> 1
-        else -> 0
+    val handValue: Int = when {
+        jokers == 5 || count[0] + jokers == 5 -> 6 // 5 of a kind
+        count[0] + jokers == 4 -> 5 // 4 of a kind
+        (count[0] == 3 && count[1] + jokers == 2) || (count[0] + jokers == 3 && count[1] == 2) -> 4 // Full house
+        count[0] + jokers == 3 -> 3 // 3 of a kind
+        count[0] == 2 && count[1] + jokers == 2 -> 2 // 2 pairs
+        count[0] + jokers == 2 -> 1 // 2 of a kind
+        else -> 0 // High card
     }
 
-    val singleCardValue: Int = value.reversed().mapIndexed { index, char ->
+    val singleCardsValue: Int = source.reversed().mapIndexed { index, char ->
         20.0.pow(index).toInt() * when {
             char.isDigit() -> char.digitToInt()
             char == 'T' -> 10
@@ -34,48 +29,28 @@ private data class Hand(val value: String, val withJokers: Boolean) {
     }.sum()
 
     fun compare(other: Hand): Int {
-        val compare = cardValue - other.cardValue
-        return if (compare == 0) {
-            singleCardValue - other.singleCardValue
-        } else {
-            compare
-        }
+        return (handValue - other.handValue).takeIf { it != 0 } ?: (singleCardsValue - other.singleCardsValue)
     }
 }
 
-private fun solveDay07a(input: String): Int {
-    val deck = input.split("\n").associate {
+private fun solveDay07(input: String, withJokers: Boolean): Int = input.split("\n")
+    .associate {
         val (hand, rank) = it.split(" ")
-        Hand(hand, false) to rank.toInt()
+        Hand(hand, withJokers) to rank.toInt()
     }
-    val sorted = deck.toSortedMap { hand1, hand2 -> hand1.compare(hand2) }
-    var sum = 0
-    sorted.onEachIndexed { index, entry ->
-        sum += (index + 1) * entry.value
+    .toSortedMap { hand1, hand2 -> hand1.compare(hand2) }
+    .entries.mapIndexed { index, entry ->
+        (index + 1) * entry.value
     }
-    return sum
-}
-
-private fun solveDay07b(input: String): Int {
-    val deck = input.split("\n").associate {
-        val (hand, rank) = it.split(" ")
-        Hand(hand, true) to rank.toInt()
-    }
-    val sorted = deck.toSortedMap { hand1, hand2 -> hand1.compare(hand2) }
-    var sum = 0
-    sorted.onEachIndexed { index, entry ->
-        sum += (index + 1) * entry.value
-    }
-    return sum
-}
+    .sum()
 
 fun main() {
     val inputExample = readFile("day07_example.txt")
     val inputTask = readFile("day07.txt")
 
-    println("Solution for task 1 example: ${solveDay07a(inputExample)}") // 6440
-    println("Solution for task 1 task:    ${solveDay07a(inputTask)}") // 253954294
-    println("Solution for task 2 example: ${solveDay07b(inputExample)}") // 5905
-    println("Solution for task 2 task:    ${solveDay07b(inputTask)}") // 254837398
+    println("Solution for task 1 example: ${solveDay07(inputExample, false)}") // 6440
+    println("Solution for task 1 task:    ${solveDay07(inputTask, false)}") // 253954294
+    println("Solution for task 2 example: ${solveDay07(inputExample, true)}") // 5905
+    println("Solution for task 2 task:    ${solveDay07(inputTask, true)}") // 254837398
 }
 
