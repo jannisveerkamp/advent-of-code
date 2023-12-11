7824537ea7e28ad24ded803917d7502ac4733004
Jannis Veerkamp <jannis.veerkamp@deepl.com>
2023-12-11 09:06:43 +0100

    Empty day 11

---
+++ 2023/src/main/kotlin/Day11.kt
@@ -0,0 +1,15 @@
+private fun solveDay11(input: String): Int {
+    return 0
+}
+
+
+fun main() {
+    val inputExample = readFile("day11_example.txt")
+    val inputTask = readFile("day11.txt")
+
+    println("Solution for task 1 example: ${solveDay11(inputExample)}") // 374
+    println("Solution for task 1 task:    ${solveDay11(inputTask)}") // ???
+    println("Solution for task 2 example: ${solveDay11(inputExample)}") // ???
+    println("Solution for task 2 task:    ${solveDay11(inputTask)}") // ???
+}
+
