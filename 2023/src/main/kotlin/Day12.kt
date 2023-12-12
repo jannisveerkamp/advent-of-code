39ca5c0ab1f02acc0516e8f8f7121453819e5ba3
Jannis Veerkamp <jannis.veerkamp@deepl.com>
2023-12-12 08:54:07 +0100

    Day 12 example data

--- 2023/src/main/kotlin/Day12.kt
+++ 2023/src/main/kotlin/Day12.kt
@@ -6,7 +6,7 @@ fun main() {
     val inputExample = readFile("day12_example.txt")
     val inputTask = readFile("day12.txt")
 
-    println("Solution for task 1 example: ${solveDay10(inputExample)}") // ???
+    println("Solution for task 1 example: ${solveDay10(inputExample)}") // 21
     println("Solution for task 1 task:    ${solveDay10(inputTask)}") // ???
     println("Solution for task 2 example: ${solveDay10(inputExample)}") // ???
     println("Solution for task 2 task:    ${solveDay10(inputTask)}") // ???
