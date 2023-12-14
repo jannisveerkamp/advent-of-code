e34dc2660ae60c06e468d8f383ae55554d0b4691
Jannis Veerkamp <jannis.veerkamp@deepl.com>
2023-12-14 08:37:31 +0100

    Day 14 part 1

--- 2023/src/main/kotlin/Day14.kt
+++ 2023/src/main/kotlin/Day14.kt
@@ -1,5 +1,30 @@
+private const val ROCK_CUBE = '#'
+private const val ROCK_ROUND = 'O'
+private const val EMPTY = '.'
+
 private fun solveDay13a(input: String): Int {
-    return 0
+    val grid = input.split("\n").map { line ->
+        line.toCharArray().toTypedArray()
+    }.toTypedArray()
+
+    var counter = 0
+    grid.forEachIndexed { y, line ->
+        line.forEachIndexed { x, char ->
+            if (char == ROCK_ROUND) {
+                var currentY = y - 1
+                var currentCounter = 0
+                while (currentY >= 0 && grid[currentY][x] != ROCK_CUBE) {
+                    if (grid[currentY][x] == EMPTY) {
+                        currentCounter++
+                    }
+                    currentY--
+                }
+                counter += grid.size - y + currentCounter
+            }
+        }
+    }
+
+    return counter
 }
 
 private fun solveDay13b(input: String): Int {
@@ -11,8 +36,8 @@ fun main() {
     val inputTask = readFile("day14.txt")
 
     println("Solution for task 1 example: ${solveDay13a(inputExample)}") // 136
-    println("Solution for task 1 task:    ${solveDay13a(inputTask)}") // ???
-    println("Solution for task 2 example: ${solveDay13b(inputExample)}") // ???
+    println("Solution for task 1 task:    ${solveDay13a(inputTask)}") // 109424
+    println("Solution for task 2 example: ${solveDay13b(inputExample)}") // 64
     println("Solution for task 2 task:    ${solveDay13b(inputTask)}") // ???
 }
 
