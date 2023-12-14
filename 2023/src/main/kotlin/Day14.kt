45702549623b2da55d7b97a4816d6050d9ee4f9f
Jannis Veerkamp <jannis.veerkamp@deepl.com>
2023-12-14 21:46:46 +0100

    Day 14 part 2

--- 2023/src/main/kotlin/Day14.kt
+++ 2023/src/main/kotlin/Day14.kt
@@ -1,43 +1,68 @@
-import common.Direction
+import common.rotate90Clockwise
 
 private const val ROCK_CUBE = '#'
 private const val ROCK_ROUND = 'O'
 private const val EMPTY = '.'
 
 private fun solveDay13a(input: String): Int {
-    val grid = input.split("\n").map { line -> line.toCharArray() }
-
-    return moveUp(grid).withIndex().sumOf { (i, row) ->
-        row.count { it == ROCK_ROUND } * (grid.size - i)
-    }
+    val grid = input.split("\n").map { line -> line.toCharArray() }.toTypedArray()
+    return grid.moveUp().totalLoad()
 }
 
-private fun Direction.next(): Direction = when (this) {
-    Direction.N -> Direction.W
-    Direction.S -> Direction.E
-    Direction.W -> Direction.S
-    Direction.E -> Direction.N
+private fun Array<CharArray>.totalLoad(): Int {
+    return withIndex().sumOf { (index, row) -> row.count { it == ROCK_ROUND } * (size - index) }
 }
 
-private fun moveUp(input: List<CharArray>): List<CharArray> {
-    val new = Array(input.size) { CharArray(input.first().size) { '.' } }
-    for (j in 0 until input.first().size) {
+private fun Array<CharArray>.moveUp(): Array<CharArray> {
+    val new = Array(size) { CharArray(first().size) { '.' } }
+    for (j in 0 until first().size) {
         var index = 0
-        for (i in input.indices) {
-            if (input[i][j] == ROCK_ROUND) {
+        for (i in indices) {
+            if (get(i)[j] == ROCK_ROUND) {
                 new[index++][j] = ROCK_ROUND
-            } else if (input[i][j] == ROCK_CUBE) {
+            } else if (get(i)[j] == ROCK_CUBE) {
                 new[i][j] = ROCK_CUBE
                 index = i + 1
             }
         }
     }
-    return new.toList()
+    return new
 }
 
 private fun solveDay13b(input: String): Int {
+    val grid = input.split("\n").map { line -> line.toCharArray() }.toTypedArray()
+
+    var currentGrid = grid
+    var cycle = 0
+    val knownPatterns: MutableMap<List<String>, Int> = mutableMapOf()
+    val maxCycles = 1_000_000_000
+
+    while (cycle < maxCycles) {
+        cycle++
+        currentGrid = currentGrid
+            .moveUp().rotate90Clockwise()
+            .moveUp().rotate90Clockwise()
+            .moveUp().rotate90Clockwise()
+            .moveUp().rotate90Clockwise()
+
+        val xy = currentGrid.map { it.concatToString() }
+        if (knownPatterns.contains(xy)) {
+            val remainingCycles = (maxCycles - cycle) % (cycle - knownPatterns[xy]!!)
+            for (i in 0 until remainingCycles) {
+                currentGrid = currentGrid
+                    .moveUp().rotate90Clockwise()
+                    .moveUp().rotate90Clockwise()
+                    .moveUp().rotate90Clockwise()
+                    .moveUp().rotate90Clockwise()
+            }
+            return currentGrid.totalLoad()
+        } else {
+            knownPatterns += xy to cycle
+        }
+    }
     return 0
 }
+
 
 fun main() {
     val inputExample = readFile("day14_example.txt")
@@ -46,6 +71,6 @@ fun main() {
     println("Solution for task 1 example: ${solveDay13a(inputExample)}") // 136
     println("Solution for task 1 task:    ${solveDay13a(inputTask)}") // 109424
     println("Solution for task 2 example: ${solveDay13b(inputExample)}") // 64
-    println("Solution for task 2 task:    ${solveDay13b(inputTask)}") // ???
+    println("Solution for task 2 task:    ${solveDay13b(inputTask)}") // 102509
 }
 
--- 2023/src/main/kotlin/common/ListExt.kt
+++ 2023/src/main/kotlin/common/ListExt.kt
@@ -5,3 +5,15 @@ fun <T> List<List<T>>.transpose(): List<List<T>> {
     forEach { list -> result.zip(list).forEach { it.first.add(it.second) } }
     return result
 }
+
+fun Array<CharArray>.transpose(): Array<CharArray> {
+    val cols = first().size
+    val rows = size
+    return Array(cols) { j ->
+        CharArray(rows) { i ->
+            get(i)[j]
+        }
+    }
+}
+
+fun Array<CharArray>.rotate90Clockwise(): Array<CharArray> = transpose().map { it.reversedArray() }.toTypedArray()
