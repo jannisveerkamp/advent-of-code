07a0437bbb62a7a6a55dc34bfa9f4e766e85eef0
Jannis Veerkamp <jannis.veerkamp@deepl.com>
2023-12-15 14:49:36 +0100

    Day 15 part 2

--- 2023/src/main/kotlin/Day15.kt
+++ 2023/src/main/kotlin/Day15.kt
@@ -1,4 +1,3 @@
-
 private fun String.customHash(): Int {
     var sum = 0
     forEach { char ->
@@ -9,8 +8,47 @@ private fun String.customHash(): Int {
 
 private fun solveDay15a(input: String): Int = input.split(",").sumOf { step -> step.customHash() }
 
+private sealed interface LenseOperation {
+    val value: String
+
+    data class Assign(override val value: String, val number: Int) : LenseOperation
+    data class Dash(override val value: String) : LenseOperation
+}
+
 private fun solveDay15b(input: String): Int {
-    return 0
+    val boxes = Array(256) { mutableListOf<LenseOperation.Assign>() }
+    val operations = input.split(",").map { operation ->
+        if (operation.contains("=")) {
+            val (first, second) = operation.split("=")
+            LenseOperation.Assign(first, second.toInt())
+        } else {
+            LenseOperation.Dash(operation.removeSuffix("-"))
+        }
+    }
+
+    operations.forEach { operation ->
+        val hash = operation.value.customHash()
+        val box = boxes[hash]
+        when (operation) {
+            is LenseOperation.Assign -> {
+                if (box.find { it.value == operation.value } != null) {
+                    box.replaceAll { if (it.value == operation.value) operation else it }
+                } else {
+                    box.add(operation)
+                }
+            }
+
+            is LenseOperation.Dash -> {
+                box.removeIf { it.value == operation.value }
+            }
+        }
+    }
+
+    return boxes.withIndex().sumOf { (indexBox, box) ->
+        box.withIndex().sumOf { (indexLense, lense) ->
+            (indexBox + 1) * (indexLense + 1) * lense.number
+        }
+    }
 }
 
 fun main() {
@@ -18,8 +56,8 @@ fun main() {
     val inputTask = readFile("day15.txt")
 
     println("Solution for task 1 example: ${solveDay15a(inputExample)}") // 1320
-    println("Solution for task 1 task:    ${solveDay15a(inputTask)}") // ???
-    println("Solution for task 2 example: ${solveDay15b(inputExample)}") // ???
-    println("Solution for task 2 task:    ${solveDay15b(inputTask)}") // ???
+    println("Solution for task 1 task:    ${solveDay15a(inputTask)}") // 510013
+    println("Solution for task 2 example: ${solveDay15b(inputExample)}") // 145
+    println("Solution for task 2 task:    ${solveDay15b(inputTask)}") // 268497
 }
 
