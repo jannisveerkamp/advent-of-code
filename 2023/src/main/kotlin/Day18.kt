d2cd4603583c0b47c295864d60b3e54a3f446bb8
Jannis Veerkamp <jannis.veerkamp@deepl.com>
2023-12-18 09:59:16 +0100

    Day 18 part 1

--- 2023/src/main/kotlin/Day18.kt
+++ 2023/src/main/kotlin/Day18.kt
@@ -1,5 +1,26 @@
-private fun solveDay18a(input: String): Int {
-    return 0
+import common.Direction
+import common.Point
+import common.shoelaceArea
+
+private fun lavaArea(instructions: List<Pair<Direction, Int>>): Long {
+    val border = instructions.sumOf { it.second }
+    val vertices = instructions.runningFold(Point(0, 0)) { last, dig -> last + dig.first.toPoint() * dig.second }
+    val interior = shoelaceArea(vertices) - (border / 2) + 1
+    return interior + border
+}
+
+private fun solveDay18a(input: String): Long {
+    val directions = input.split("\n").map { line ->
+        val (dir, amount) = line.split(" ")
+        when (dir) {
+            "U" -> Direction.N
+            "D" -> Direction.S
+            "L" -> Direction.W
+            "R" -> Direction.E
+            else -> error("Unknown direction: $dir")
+        } to amount.toInt()
+    }
+    return lavaArea(directions)
 }
 
 private fun solveDay18b(input: String): Int {
@@ -11,8 +32,8 @@ fun main() {
     val inputTask = readFile("day18.txt")
 
     println("Solution for task 1 example: ${solveDay18a(inputExample)}") // 62
-    println("Solution for task 1 task:    ${solveDay18a(inputTask)}") // ???
-    println("Solution for task 2 example: ${solveDay18b(inputExample)}") // ???
-    println("Solution for task 2 task:    ${solveDay18b(inputTask)}") // ???
+    println("Solution for task 1 task:    ${solveDay18a(inputTask)}") // 53300
+//    println("Solution for task 2 example: ${solveDay18b(inputExample)}") // ???
+//    println("Solution for task 2 task:    ${solveDay18b(inputTask)}") // ???
 }
 
--- 2023/src/main/kotlin/common/Point.kt
+++ 2023/src/main/kotlin/common/Point.kt
@@ -4,8 +4,11 @@ import kotlin.math.abs
 
 data class Point(val x: Int, val y: Int) {
     fun manhattan(other: Point): Int = abs(other.x - x) + abs(other.y - y)
+
     operator fun plus(other: Point): Point = Point(x + other.x, y + other.y)
     operator fun minus(other: Point): Point = Point(x - other.x, y - other.y)
+    operator fun times(other: Int): Point = Point(x * other, y * other)
+
     val down: Point get() = Point(x, y + 1)
     val up: Point get() = Point(x, y - 1)
     val left: Point get() = Point(x - 1, y)
@@ -19,3 +22,9 @@ data class PointL(val x: Long, val y: Long) {
 data class Point3(val x: Int, val y: Int, val z: Int) {
     fun manhattan(other: Point3): Int = abs(other.x - x) + abs(other.y - y) + abs(other.z - z)
 }
+
+fun shoelaceArea(vertices: List<Point>): Long = vertices.indices.sumOf { i ->
+    val (x1, y1) = vertices[i]
+    val (x2, y2) = vertices[(i + 1) % vertices.size]
+    x1.toLong() * y2 - y1.toLong() * x2
+} / 2
