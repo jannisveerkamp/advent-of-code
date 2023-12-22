90f6f397905dce058fab52f8d69d1162be029799
Jannis Veerkamp <jannis.veerkamp@deepl.com>
2023-12-22 16:40:56 +0100

    Empty day 23 and 24

---
+++ 2023/src/main/kotlin/Day23.kt
@@ -0,0 +1,18 @@
+private fun solveDay23a(input: String): Int {
+    return 0
+}
+
+private fun solveDay23b(input: String): Int {
+    return 0
+}
+
+fun main() {
+    val inputExample = readFile("day23_example.txt")
+    val inputTask = readFile("day23.txt")
+
+    println("Solution for task 1 example: ${solveDay23a(inputExample)}") // ???
+    println("Solution for task 1 task:    ${solveDay23a(inputTask)}") // ???
+    println("Solution for task 1 example: ${solveDay23b(inputExample)}") // ???
+    println("Solution for task 1 task:    ${solveDay23b(inputTask)}") // ???
+}
+
---
+++ 2023/src/main/kotlin/Day24.kt
@@ -0,0 +1,18 @@
+private fun solveDay24a(input: String): Int {
+    return 0
+}
+
+private fun solveDay24b(input: String): Int {
+    return 0
+}
+
+fun main() {
+    val inputExample = readFile("day24_example.txt")
+    val inputTask = readFile("day24.txt")
+
+    println("Solution for task 1 example: ${solveDay24a(inputExample)}") // ???
+    println("Solution for task 1 task:    ${solveDay24a(inputTask)}") // ???
+    println("Solution for task 1 example: ${solveDay24b(inputExample)}") // ???
+    println("Solution for task 1 task:    ${solveDay24b(inputTask)}") // ???
+}
+
