d8b6dd89663b18c353e6ab5c68d6b08b5a5075c7
Jannis Veerkamp <jannis.veerkamp@deepl.com>
2025-12-11 11:08:38 +0100

    Day 11 part 2

--- 2025/src/main/kotlin/Day11.kt
+++ 2025/src/main/kotlin/Day11.kt
@@ -5,23 +5,21 @@ private fun parseDay11(input: String): List<Rack> = input.lines().map { line ->
 
 private data class Rack(val input: String, val outputs: List<String>)
 
-private fun findNext(input: String, racks: List<Rack>): Long {
+private val knownPaths = mutableMapOf<String, Long>()
+
+private fun findNext(input: String, racks: List<Rack>, visitsLeft: List<String>): Long {
     return racks.filter { it.input == input }.map { it.outputs }.flatten().sumOf { next ->
         when {
-            next == "out" -> 1
-            else -> findNext(next, racks)
+            next == "out" -> if (visitsLeft.isEmpty()) 1 else 0
+            else -> knownPaths[next + visitsLeft.joinToString("")] ?: findNext(next, racks, visitsLeft - next).apply {
+                knownPaths[next + visitsLeft.joinToString("")] = this
+            }
         }
     }
 }
 
-private fun solveDay11a(input: String): Long {
-    val racks = parseDay11(input)
-    return findNext("you", racks)
-}
-
-private fun solveDay11b(input: String): Int {
-    return 0
-}
+private fun solveDay11a(input: String): Long = findNext("you", parseDay11(input), emptyList())
+private fun solveDay11b(input: String): Long = findNext("svr", parseDay11(input), listOf("dac", "fft"))
 
 fun main() {
     val inputExample = readFile("day11_example.txt")
@@ -29,7 +27,7 @@ fun main() {
 
     println("Solution for task 1 example: ${solveDay11a(inputExample)}") // 5
     println("Solution for task 1 task:    ${solveDay11a(inputTask)}") // 613
-    println("Solution for task 1 example: ${solveDay11b(inputExample)}") // ???
-    println("Solution for task 1 task:    ${solveDay11b(inputTask)}") // ???
+    println("Solution for task 2 example: ${solveDay11b(inputExample)}") // 2
+    println("Solution for task 2 task:    ${solveDay11b(inputTask)}") // 372918445876116
 }
 
